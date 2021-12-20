901828a8a7fdc4e5ed0e43c2d93b022b3bfef630
caro.deman20 <caro.deman20@triana.salesianos.edu>
2021-12-20 09:29:46 +0100

    iniciarProyecto

---
+++ miniproyectoFinal/src/main/java/com/minproyecto/miniproyectoFinal/MiniproyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.minproyecto.miniproyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniproyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniproyectoFinalApplication.class, args);
+	}
+
+}
---
+++ miniproyectoFinal/src/test/java/com/minproyecto/miniproyectoFinal/MiniproyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minproyecto.miniproyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniproyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
